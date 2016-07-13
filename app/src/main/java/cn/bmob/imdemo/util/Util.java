ed81d74e73e2e4e2ff9cb7ab188a225be7464eda
wmmmm <1472610316@qq.com>
2016-07-13 12:18:01 +0800

    初步的样子
    只有菜单栏，功能未实现

---
+++ app/src/main/java/cn/bmob/imdemo/util/Util.java
@@ -0,0 +1,27 @@
+package cn.bmob.imdemo.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+/**
+ * @author smile
+ * @project Util
+ * @date 2016-03-01-14:55
+ */
+public class Util {
+    public static boolean checkSdCard() {
+        if (android.os.Environment.getExternalStorageState().equals(
+                android.os.Environment.MEDIA_MOUNTED))
+            return true;
+        else
+            return false;
+    }
+}
