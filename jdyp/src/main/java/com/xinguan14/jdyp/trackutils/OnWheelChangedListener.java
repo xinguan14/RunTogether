ca94771ecedc219743cb488933001e768e6bf6da
Kobe_Bryant <1032367864@qq.com>
2016-10-13 17:38:01 +0800

    完善：跑步界面，去除百度鹰眼多余的button

--- jdyp/src/main/java/com/xinguan14/jdyp/trackutils/OnWheelChangedListener.java
+++ jdyp/src/main/java/com/xinguan14/jdyp/trackutils/OnWheelChangedListener.java
@@ -1,5 +1,5 @@
 package com.xinguan14.jdyp.trackutils;
 
 public interface OnWheelChangedListener {
-    public void onChanged(WheelView wheel, int oldValue, int newValue);
+    void onChanged(WheelView wheel, int oldValue, int newValue);
 }
--- jdyp/src/main/java/com/xinguan14/jdyp/trackutils/OnWheelScrollListener.java
+++ jdyp/src/main/java/com/xinguan14/jdyp/trackutils/OnWheelScrollListener.java
@@ -1,7 +1,7 @@
 package com.xinguan14.jdyp.trackutils;
 
 public interface OnWheelScrollListener {
-    public void onScrollingStarted(WheelView wheel);
+     void onScrollingStarted(WheelView wheel);
 
-    public void onScrollingFinished(WheelView wheel);
+     void onScrollingFinished(WheelView wheel);
 }
